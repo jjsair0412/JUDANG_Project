8d282d3566a25cdef7f92923bc9dc4caf7f498df
주진성 <jjsair0412@naver.com>
2021-09-15 21:21:11 +0900

    Initial commit

---
+++ src/main/java/hello/JuDang/JUDANG/JudangApplication.java
@@ -0,0 +1,13 @@
+package hello.JuDang.JUDANG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JudangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JudangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/JuDang/JUDANG/JudangApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.JuDang.JUDANG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
