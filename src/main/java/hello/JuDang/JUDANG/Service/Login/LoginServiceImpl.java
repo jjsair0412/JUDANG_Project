cf3b02b3b29db7384cbaf9358214f7863114ec87
kjb97 <qkqqh97@naver.com>
2021-09-30 03:01:04 +0900

    로그인 구현중

--- src/main/java/hello/JuDang/JUDANG/Service/Login/LoginServiceImpl.java
+++ src/main/java/hello/JuDang/JUDANG/Service/Login/LoginServiceImpl.java
@@ -3,14 +3,22 @@ package hello.JuDang.JUDANG.Service.Login;
 import hello.JuDang.JUDANG.Domain.Member;
 import hello.JuDang.JUDANG.Repository.Login.LoginRepository;
 
+import java.util.List;
+
 public class LoginServiceImpl implements LoginService{
     private LoginRepository loginRepository;
 
     @Override
     public Member login(Member member) {
-        Member loginMember = loginRepository.select(member.getId(), member.getPassword());
+        List<Member> buyerLogin = loginRepository.selectBuyer(member);
+        List<Member> sellerLogin = loginRepository.selectSeller(member);
+
+        if(buyerLogin.isEmpty()){
+            return sellerLogin.get(0);
+        }else if(sellerLogin.isEmpty()){
+            return buyerLogin.get(0);
+        }else  return null;
 
 
-        return null;
     }
 }
