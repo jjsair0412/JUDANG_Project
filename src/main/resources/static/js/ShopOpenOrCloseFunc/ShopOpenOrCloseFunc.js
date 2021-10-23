3f9b6b6cb00a2300d3f54b58463aaa70b8c26879
주진성 <jjsair0412@naver.com>
2021-10-24 01:35:57 +0900

    가게 열고 닫는 체크박스 구현 완료 ( 컨트롤러부분까지 완료 )

---
+++ src/main/resources/static/js/ShopOpenOrCloseFunc/ShopOpenOrCloseFunc.js
@@ -0,0 +1,46 @@
+let storeCheckBoxIds = { // 체크박스에 동적으로 할당시킬 id값들 저장해두고 있는 객체
+    'checkBoxId': 0
+}
+
+let a = 0;
+
+if (myShops.length === 1) {
+    $('#myshopsInfoPosition').append('<div>가게가 없어요. 먼저 등록해주세요.</div>')
+} else {
+    $('#myshopsInfoPosition').empty();
+    for (let i = 0; i < myShops.length; i++) {
+        const nowShop = myShops[i].shopName;
+        storeCheckBoxIds.checkBoxId = +a;
+        $('#myshopsInfoPosition').append('가게 이름 : <div>' + myShops[i].shopName + '</div>')
+        $('#myshopsInfoPosition').append('가게 카테고리 : <div>' + myShops[i].category + '</div>')
+        $('#myshopsInfoPosition').append('전체 좌석수 : <div>' + myShops[i].totalSeat + '</div>')
+        $('#myshopsInfoPosition').append('현재 좌석수 : <div>' + myShops[i].currentSeat + '</div>')
+        $('#myshopsInfoPosition').append('<input type="checkbox" id="'+storeCheckBoxIds.checkBoxId+'" onclick="openLogic(\'' + a + '\',\''+nowShop+'\');">')
+
+        ++a;
+
+        function openLogic(id, shopName) {
+            let checked = document.getElementById(eval("id")).checked;
+            let openShop = new Object();
+            openShop.shopName = shopName;
+            openShop.isOpen = checked;
+            $.ajax({
+                type: "get",
+                url: "/SellerPage/openCloseFunc",
+                data: openShop,
+                success: function (result) {
+                    console.log(result);
+                    if (result == 1) {
+                        alert("열고닫기 완료")
+                        location.reload();
+                    } else {
+                        alert("가게 열고 닫기 실패")
+                    }
+                },
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    alert("통신 실패.")
+                }
+            });
+        }
+    }
+}
