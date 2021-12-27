ba839124356f0f977ae7ed3810c41799e778b211
kjb97 <qkqqh97@naver.com>
2021-12-27 15:16:56 +0900

    검증 추가중

--- src/main/java/hello/JuDang/JUDANG/Controller/Main/MainController.java
+++ src/main/java/hello/JuDang/JUDANG/Controller/Main/MainController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
