c840681210aeed82f949106176c30d41a362ab09
kjb97 <qkqqh97@naver.com>
2022-01-05 23:47:05 +0900

    Join MemberForm 빈 검증

--- src/main/java/hello/JuDang/JUDANG/Domain/Member.java
+++ src/main/java/hello/JuDang/JUDANG/Domain/Member.java
@@ -4,7 +4,8 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class Member {
     private String id;
     private String password;
@@ -12,4 +13,17 @@ public class Member {
     private String email;
     private Integer age;
     private UserType userType;
+
+    public Member(){
+
+    }
+
+    public Member(String id, String password, String name, String email, Integer age, UserType userType) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.userType = userType;
+    }
 }
